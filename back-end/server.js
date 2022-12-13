7dc867d096b2bb1f3e879f079252f83eff2f7ca0
ambernluu <ambernluu@gmail.com>
2022-12-13 09:19:19 -0500

    change port to accept dynamic

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const app = require("./app");
-const { PORT } = require("./config");
+// const { PORT } = require("./config");
 
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:5000`);
-});
+// app.listen(PORT, function () {
+//   console.log(`Started on http://localhost:5000`);
+// });
+
+app.listen(process.env.PORT || 5000)
